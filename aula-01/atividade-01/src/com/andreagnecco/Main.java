908b6d063a44629f12e19bbc3813b0951d0f2a6d
andreagnecco <andreagnecco@gmail.com>
2021-04-29 19:14:47 -0300

    Modifica atividade-01.

--- aula-01/atividade-01/src/com/andreagnecco/Main.java
+++ aula-01/atividade-01/src/com/andreagnecco/Main.java
@@ -22,19 +22,6 @@ public class Main {
 
         System.out.printf("Seu IMC é: %.2f.\n", imc);
 
-        if (imc >= 40) {
-            System.out.println("Seu IMC é Obesidade Classe 3.");
-        } else if (imc>=35) {
-            System.out.println("Seu IMC é Obesidade Classe 2.");
-        } else if (imc >=30) {
-            System.out.println("Seu IMC é OBesidade Classe 1.");
-        } else if (imc >=25) {
-            System.out.println("Seu IMC indica excesso de peso.");
-        } else if (imc >=18.5) {
-            System.out.println("Seu IMC indica peso normal.");
-        } else {
-            System.out.println("Seu IMC indica baixo peso.");
-        }
 
 
     }
